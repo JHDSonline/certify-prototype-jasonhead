d06a7732951c588d4e48f2c8d1f6c23cfb1800c3
Jared Cunha <jcunha@omb.eop.gov>
2018-02-28 11:45:22 -0500

    update for review

--- _site/assets/cds-stuff.js
+++ _site/assets/cds-stuff.js
@@ -1,2 +1,3 @@
-<script type="text/javascript" src="/certify-prototype/assets/cds.js"></script>
-<img src="/certify-prototype/assets/svg-sprite/sprite.svg">
+<link rel="stylesheet" type="text/css" href="/assets/cds.css">
+<script type="text/javascript" src="/assets/cds.js"></script>
+<img src="/assets/svg-sprite/sprite.svg">
