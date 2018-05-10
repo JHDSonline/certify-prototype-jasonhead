6b893258104db2f38638e779784349fc3020eb08
Jared Cunha <jcunha@omb.eop.gov>
2018-05-10 09:43:41 -0400

    update deploy method

--- _site/assets/cds-stuff.js
+++
@@ -1,3 +0,0 @@
-<link rel="stylesheet" type="text/css" href="/certify-prototype/assets/cds.css">
-<script type="text/javascript" src="/certify-prototype/assets/cds.js"></script>
-<img src="/certify-prototype/assets/svg-sprite/sprite.svg">
