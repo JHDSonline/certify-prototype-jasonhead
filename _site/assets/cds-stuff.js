bdbb45b5a43447e64ab0b25379f87a082112c960
Jared Cunha <jcunha@omb.eop.gov>
2018-05-10 13:57:12 -0400

    update the ignore

--- _site/assets/cds-stuff.js
+++
@@ -1,3 +0,0 @@
-<link rel="stylesheet" type="text/css" href="/certify-prototype/assets/cds.css">
-<script src="/certify-prototype/assets/cds.js" type="text/javascript"></script>
-<img src="/certify-prototype/assets/svg-sprite/sprite.svg">
