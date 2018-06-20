bf7618ba233ac2672048398f5e61d79e5b38c987
Jared Cunha <jcunha@omb.eop.gov>
2018-06-20 14:49:58 -0400

    fix hashes in use with url params

--- assets/javascripts/components/store-url-params.js
+++ assets/javascripts/components/store-url-params.js
@@ -1,5 +1,4 @@
 /*
-
   Store URL Params:
   This function will store URL params into sessionStorage for use with prototypes.
 */
@@ -10,7 +9,7 @@ storeUrlParams = function () {
   for(var i = 0; i < hashes.length; i++)
   {
     hash = hashes[i].split('=');
-    sessionStorage.setItem(hash[0], decodeURI(hash[1]));
+    sessionStorage.setItem(hash[0], decodeURI(hash[1]).replace("#",""));
   }
 }
 
